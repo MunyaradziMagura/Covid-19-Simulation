dbd6f9d4b3eb1e4b643d470ed5ac802b82a873cc
MunyaradziMagura <43807813+MunyaradziMagura@users.noreply.github.com>
2022-03-10 16:18:18 +1030

    get population size

---
+++ simulation.py
@@ -0,0 +1,15 @@
+def getPopulation():
+    populationSize = input("enter a population size: ")
+    # ensure the population size is a number
+    while populationSize.isnumeric() != True:
+        print("\nplease enter a valid size. only whole number integers \n")
+        populationSize = input("enter a population size: ")
+
+    return populationSize
+
+
+def main():
+    populationSize = getPopulation()
+
+
+main()
